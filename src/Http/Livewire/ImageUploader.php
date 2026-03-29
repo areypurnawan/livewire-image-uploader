e36fbc7e3e30ecaedc8f86f9979fc5f2a92a065c
areypurnawan <164567382+areypurnawan@users.noreply.github.com>
2026-03-29 14:05:17 +0700

    Refactor ImageUploader to include new properties

--- src/Http/Livewire/ImageUploader.php
+++ src/Http/Livewire/ImageUploader.php
@@ -4,6 +4,7 @@ namespace Sherwinchia\LivewireImageUploader\Http\Livewire;
 
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class ImageUploader extends Component
@@ -12,7 +13,7 @@ class ImageUploader extends Component
     public $rawImages;
     public $images = [];
     public $imagesName = [];
-    public $oldImages, $multiple, $name, $size;
+    public $multiple, $name, $size, $label, $publicPath, $publicPathView;
 
     protected $messages = [
         'rawImages.*.image' => 'The images format must be type of image.',
@@ -23,18 +24,20 @@ class ImageUploader extends Component
         'rawImages.max' => 'The image must not be greater than :max KB.',
     ];
 
-    public function mount(string $name, bool $multiple = false, int $size=1024, array $old = null)
+    public function mount(string $name, bool $multiple = false, int $size = 1024, string $label = null, $publicPath = 'public/image-uploader/', $publicPathView = null)
     {
         $this->name = $name;
         $this->size = $size;
+        $this->label = $label;
         $this->multiple = $multiple;
-        $multiple ? $this->rawImages=[] : $this->rawImages = null;
-        $old ? $this->oldImages = $old : $this->oldImages = null;
+        $multiple ? $this->rawImages = [] : $this->rawImages = null;
+        $this->publicPath = $publicPath;
+        $this->publicPathView = $publicPathView;
     }
 
     public function updatingRawImages()
     {
-        $this->multiple ? $this->rawImages=[] : $this->rawImages = null;
+        $this->multiple ? $this->rawImages = [] : $this->rawImages = null;
         $this->images = array();
     }
 
@@ -42,16 +45,16 @@ class ImageUploader extends Component
     {
         if ($this->multiple) {
             $this->validate(
-                ['rawImages.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:'.$this->size],
+                ['rawImages.*' => 'image|mimes:jpeg,png,jpg,gif,svg,webp|max:' . $this->size],
             );
-        } 
-      
+        }
+
         if (!$this->multiple) {
             $this->validate(
-                ['rawImages' => 'image|mimes:jpeg,png,jpg,gif,svg|max:'.$this->size],
+                ['rawImages' => 'image|mimes:jpeg,png,jpg,gif,svg,webp|max:' . $this->size],
                 []
             );
-        } 
+        }
 
         // $this->images = $value;
         $this->multiple ? $this->images = $value : $this->images = array($value);
@@ -62,37 +65,38 @@ class ImageUploader extends Component
 
     public function uploadImages()
     {
+
         if (!empty($this->imagesName)) {
             foreach ($this->imagesName as $image) {
-                Storage::delete('public/image-uploader/' . $image);
+                $this->dispatch('deleteImage', $image, $this->publicPath);
+                Storage::delete($this->publicPath .'/' . $this->publicPathView . '/' . $image);
             }
             $this->imagesName = array();
         }
 
         foreach ($this->images as $image) {
-            $image->store('public/image-uploader');
+
+            $image->store($this->publicPath.'/' . $this->publicPathView);
             array_push($this->imagesName, $image->hashName());
         }
+
         return $this->handleImagesUpdated();
     }
 
     public function handleRemoveImage($index, $old = false)
     {
-        if ($old) {
-            $this->emitUp('deleteImage', $this->oldImages[$index]);
-            // $this->oldImages[$index]->delete();
-            unset($this->oldImages[$index]);
-        } else {
-            Storage::delete("public/image-uploader/" . $this->imagesName[$index]);
+        
+            Storage::delete($this->publicPath.'/' . $this->publicPathView . '/' . $this->imagesName[$index]);
+            $this->dispatch('deleteImage', $this->imagesName[$index], $this->publicPath);
             unset($this->images[$index]);
             unset($this->imagesName[$index]);
-            return $this->handleImagesUpdated();
-        }
+           // return $this->handleImagesUpdated();
     }
 
     public function handleImagesUpdated()
     {
         $this->dispatch('imagesUpdated', $this->name, $this->imagesName);
+        // $this->dispatch('imagesUpdated', ['id' => $this->name]);
     }
 
     public function render()
